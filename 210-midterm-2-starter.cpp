3ee27c49a300013e2fdba21359c53ca5de5e890c
Smarika Rai <smarikarai@mac.attlocal.net>
2025-10-19 20:43:44 -0700

    really struggling

--- 210-midterm-2-starter.cpp
+++ 210-midterm-2-starter.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
@@ -204,7 +209,7 @@ public:
 };
 
 int main() {
-    cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS;  // dummy statement to avoid compiler warning
+    cout << "coffee line sim (stub)\n";// dummy statement to avoid compiler warning
 
     
     return 0;
